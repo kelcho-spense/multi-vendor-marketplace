f18c5971b0c2ec06dba521f0a85baedcac2d53b9
kelcho-spense <kelchospense88@gmail.com>
2026-01-23 00:05:17 +0300

    refactor: improve formatting and indentation in auth guard classes

--- api/src/auth/guards/jwt-auth.guard.ts
+++ api/src/auth/guards/jwt-auth.guard.ts
@@ -5,22 +5,22 @@ import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super();
-  }
-
-  canActivate(context: ExecutionContext) {
-    // Check if route is marked as public
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (isPublic) {
-      return true;
+    constructor(private reflector: Reflector) {
+        super();
     }
 
-    // Otherwise, use the default JWT auth guard behavior
-    return super.canActivate(context);
-  }
+    canActivate(context: ExecutionContext) {
+        // Check if route is marked as public
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (isPublic) {
+            return true;
+        }
+
+        // Otherwise, use the default JWT auth guard behavior
+        return super.canActivate(context);
+    }
 }
